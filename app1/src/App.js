ea82fbbfe249fa5da55dbbc773ac2471a8ca600f
mannieshub <110074002+mannieshub@users.noreply.github.com>
2023-08-04 14:28:27 +0700

    Initial commit

---
+++ app1/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://www.tiktok.com/@nirvxsh/video/7259412610909572357?is_from_webapp=1&sender_device=pc' className="App-logo" alt="logo" />
+        <p>
+          Tiktoker ชื่อดัง ย่านพระราม7
+          พ่อน้องออนิว แล้วใครคือแม่น้องออนิว
+        </p>
+        <a
+          className="App-link"
+          href="https://www.tiktok.com/@nirvxsh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Nirvash 300follower NOW!!!
+        รับชมได้แล้ววันนี้
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
