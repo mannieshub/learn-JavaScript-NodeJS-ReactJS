2c567ae0b41d9656728d3f8be575bd0627be49b7
mannieshub <110074002+mannieshub@users.noreply.github.com>
2023-08-04 15:55:36 +0700

    css

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,23 +1,39 @@
 import './App.css';
 import logo from './bb.png'
+import './style.css'
+
 
 function App() {
+
+  const divstyle={
+    color:'red',
+    backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'3px'
+  }
+
   return (
-    <div className="App">
+    <>
+      <h2>Test External CSS</h2>
+      <div className='title'>React & React Native</div>
+    </>
+  )
+
+  return (
+    <div style={divstyle} className="App">
+      อะไรเอ่ยเทส divstyle
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tiktoker ชื่อดัง ย่านพระราม7
-          พ่อน้องออนิว แล้วใครคือแม่น้องออนิว
-        </p>
+        <h3>มังกรฟ้า</h3>
+
         <a
           className="App-link"
           href="https://www.tiktok.com/@nirvxsh"
           target="_blank"
           rel="noopener noreferrer"
         >
-        Nirvash 300follower NOW!!!
-        รับชมได้แล้ววันนี้
+        Nirvash reach 300 follower NOW!!!
+        คลิกเพื่อสนับสนุนเสฐนันท์ โมซาลียานนท์
         </a>
       </header>
     </div>
